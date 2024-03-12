450321f77464bfb8426aac7df941d73d48f8eec7
Aurel_B_ <aurel.burja@eshopwedrop.com>
2024-03-13 00:11:21 +0200

    lab2 forms

---
+++ src/main/java/ro/ulbs/paradigme/lab2/forms/Form.java
@@ -0,0 +1,34 @@
+package ro.ulbs.paradigme.lab2.forms;
+
+public class Form {
+    private String color;
+
+    public Form() {
+        color = "white";
+    }
+
+    public Form(String color) {
+        this.color = color;
+    }
+
+    public float getArea() {
+        return 0;
+    }
+
+    public String toString() {
+        return "This form has the color "+color;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Form) {
+            Form f = (Form) obj;
+            if (this.color == null && f.color == null) {
+                return true;
+            }
+            else if (this.color != null && f.color != null && this.color.compareTo(f.color) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
